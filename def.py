496dc34de7b1b136971f5e3ad59440899c4b9201
Yuval <copycatfn308@gmail.com>
2020-11-02 22:24:06 +0200

    v1.5

--- def.py
+++ def.py
@@ -3,15 +3,32 @@
 import requests, math
 from bs4 import BeautifulSoup
 from currency_converter import CurrencyConverter
-import csv
+import csv, os
+from colorama import Fore
 
 
 url = input("Please enter ebay's url: ")
 budget = int(input("Please enter your product's budget in USD: "))
 currency = input('Please enter your currency in caps: ')
 c = CurrencyConverter()
+prices_list = []
+average_lower = False
+
+
+def average(product):
+    global prices_list, average_lower
+    Sum = sum(prices_list)
+    amount = len([Int for Int in prices_list if isinstance(Int, int)])
+    average = Sum / amount
+    if product < average:
+        average_lower = True
+    else:
+        pass
+
 
 def finder(currency, budget, url):
+    global prices_list, average_lower
+    
     req = requests.get(f'{url}&_ipg=200')
     soup = BeautifulSoup(req.text, 'lxml')
     div = soup.find('div', {'id': "srp-river-results"})
@@ -32,27 +49,33 @@ def finder(currency, budget, url):
 
             convert = c.convert(p, f'{currency}', 'USD')
             price = math.trunc(convert)
+            prices_list.append(price)
 
             if price < budget:
                 print(f'{name.text} ---> Price:{price} USD ---> ' + link['href'])
-                yep = True
+                worth = True
             
             else:
-                yep = False
+                worth = False
                 pass
 
+            average(price)
             with open('products.csv', "a+", newline='') as f:
                 w = csv.writer(f)
-                w.writerow([f"{name.text}", f"{price}", f"{link['href']}", yep])
+                w.writerow([f"{name.text}", f"{price}", f"{link['href']}", worth, average_lower])
 
             f.close()
 
 
+
+
+
 if __name__ == '__main__':
+    os.remove('products.csv')
 
     with open('products.csv', "a+", newline='') as File:
         w = csv.writer(File)
-        w.writerow(["Product", "Price", "Link", "Worth?"])
+        w.writerow(["Product", "Price", "Link", "Worth?, Average Lower?"])
         w.writerow([" "])
     File.close()
 
